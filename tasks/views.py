6c3fb20f0f9fcecc2df839808b4bb4a60700bb40
MSzarek <mszarek@student.agh.edu.pl>
2018-06-11 16:36:47 +0200

    Ready to go!

--- tasks/views.py
+++ tasks/views.py
@@ -10,7 +10,6 @@ from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
-
 @login_required(login_url='/login/')
 def index(request):
     headings = TaskCategory.objects.all()
